9a4a05ed1ebfc9b4614e5f38a985c48c15d3989d
vishalicious213 <vishalicious213@gmail.com>
2025-10-17 13:13:20 -0700

    server.js: had express use urlencoded to get form data

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+// urlencoded tells express to extract data from a form & add it
+// to the body property on the request object
+app.use(express.urlencoded({ extended: true }))
+
 app.listen(3000, function () {
     console.log('Listening on port 3000')
 })
@@ -12,6 +16,7 @@ app.get("/", (req, res) => {
 
 app.post("/quotes", (req, res) => {
     console.log("Posting to /quotes")
+    console.log(req.body)
 })
 
 // console.log("May Node be with you")
