383a306adbee42ea90b58ee28c662cb4af204104
vishalicious213 <vishalicious213@gmail.com>
2025-10-17 13:38:41 -0700

    server.js: installed mongodb & connected it to db on cloud atlas

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
 const app = express()
 
 // urlencoded tells express to extract data from a form & add it
 // to the body property on the request object
 app.use(express.urlencoded({ extended: true }))
+
+
+// mongodb+srv://vishalicious213_db_user:9aPRILVehFopzIju@cluster0.4srl9qn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+// MongoClient.connect('mongodb+srv://vishalicious213_db_user:9aPRILVehFopzIju@cluster0.4srl9qn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', (err, client) => {
+//     console.log("Mongo connected to database")
+// })
+
+MongoClient.connect('mongodb+srv://vishalicious213_db_user:9aPRILVehFopzIju@cluster0.4srl9qn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(
+    console.log("Connected to database")
+).catch(error => console.error(error))
+
 
 app.listen(3000, function () {
     console.log('Listening on port 3000')
