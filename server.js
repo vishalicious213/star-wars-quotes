170a0c2f5cea7ae796286cc3794b2332fb0774f1
vishalicious213 <vishalicious213@gmail.com>
2025-10-17 12:39:49 -0700

    updated request handler for / to serve index.html

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 
 app.listen(3000, function () {
-    console.log("Listening on port 3000")
+    console.log('Listening on port 3000')
 })
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    // res.send("Hello World")
+    res.sendFile(__dirname + `/index.html`)
 })
 
 
 console.log("May Node be with you")
+console.log(__dirname)
