851bdc0fd185244778c86d0c01b5400c0e26d174
vishalicious213 <vishalicious213@gmail.com>
2026-04-21 21:34:05 -0700

    index.js: added event listener for deleteButton

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button')
 
 update.addEventListener('click', _ => {
     fetch('/quotes', {
@@ -8,6 +9,21 @@ update.addEventListener('click', _ => {
             name: 'Darth Vader', 
             quote: 'I find your lack of faith disturbing.' 
         })
+    })
+    .then(res => {
+        if (res.ok) return res.json()
+    })
+    .then(response => {
+        // console.log(response)
+        window.location.reload(true)
+    })
+})
+
+deleteButton.addEventListener('click', _ => {
+    fetch('/quotes', {
+        method: 'delete',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: 'Darth Vader' })
     })
     .then(res => {
         if (res.ok) return res.json()
